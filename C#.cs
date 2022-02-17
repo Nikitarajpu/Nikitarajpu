5b912d1728f842e010dd945e7e241a1f91ab635a
Nikitarajpu <99063004+Nikitarajpu@users.noreply.github.com>
2022-02-17 09:53:11 +0530

    Create C#.cs

---
+++ C#.cs
@@ -0,0 +1,32 @@
+- 👋 Hi, I’m @Nikitarajpu
+- 👀 I’m interested in ...
+- 🌱 I’m currently learning ...
+- 💞️ I’m looking to collaborate on ...
+- 📫 How to reach me ...
+
+<!---
+Nikitarajpu/Nikitarajpu is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CSharp_Shell
+{
+
+ class Program 
+{
+     static void Main()
+  {
+           
+    //int num,i;
+  Console.WriteLine("entre the number");
+ int num=Convert.ToInt32(Console.ReadLine());
+    for(int i=1;i<=num;i++)
+    {
+    	Console.WriteLine("each one,teach one,tree one");
+     }
+  }
+}
+}
